1786e81818d282cead71e935071da70d1f387342
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-06 02:03:55 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,5 +2,5 @@ def parrot(bird = "Squawk!")
   puts bird
   return bird
 end
-)# Create method `parrot` that outputs a given phrase and
+# Create method `parrot` that outputs a given phrase and
 # returns the phrase
